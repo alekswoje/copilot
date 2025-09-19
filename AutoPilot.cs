68cc642568c3a2339511cb323a7df92a57f09155
Aleks Wojewoda <alekswojewoda@live.com>
2025-09-18 18:35:14 -0700

    Add System.Windows.Forms namespace to AutoPilot for UI enhancements

--- AutoPilot.cs
+++ AutoPilot.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Windows.Forms;
 using ExileCore;
 using ExileCore.PoEMemory;
 using ExileCore.PoEMemory.Components;
